b0e2c890393b65439dda1f551132058f5f9f785a
Gayathri R <iamanifs@gmail.com>
2018-03-04 23:23:33 +0530

    All done except mailing app

--- app/src/main/java/com/example/gayathri/resume/MainActivity.java
+++ app/src/main/java/com/example/gayathri/resume/MainActivity.java
@@ -13,6 +13,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -28,28 +30,78 @@ public class MainActivity extends AppCompatActivity {
         ActionBar actionbar = getSupportActionBar();
         actionbar.setDisplayHomeAsUpEnabled(true);
         actionbar.setHomeAsUpIndicator(R.drawable.ic_menu);
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
 
         mDrawerLayout = findViewById(R.id.drawer_layout);
 
         NavigationView nav = findViewById(R.id.nav_view);
+        final AboutFragment frag1 = new AboutFragment();
+        fragmentTransaction.add(R.id.frag1,frag1);
+        fragmentTransaction.commit();
         nav.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                FragmentManager fragmentManager = getSupportFragmentManager();
+                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
                 switch(item.getItemId()) {
                     case R.id.about:
                         mDrawerLayout.closeDrawer(GravityCompat.START);
-                        //Intent intent = new Intent(Intent.ACTION_VIEW,Uri.parse("http://www.facebook.com/keeru8999"));
-                        ///*getApplicationContext().*/startActivity(Intent.createChooser(intent,"Poda echa"));
-                        Intent callIntent = new Intent(Intent.ACTION_DIAL,Uri.parse("tel:+919840249483"));
+
+                        /*Intent callIntent = new Intent(Intent.ACTION_DIAL,Uri.parse("tel:+919840249483"));
                         callIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-                        startActivity(callIntent);
+                        startActivity(callIntent);*/
+                        fragmentTransaction.replace(R.id.frag1,frag1);
+                        fragmentTransaction.addToBackStack(null);
+                        fragmentTransaction.commit();
                         return true;
                     case R.id.edu :
                         mDrawerLayout.closeDrawer(GravityCompat.START);
-                        FragmentManager fragmentManager = getSupportFragmentManager();
-                        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
                         EducationFragment fragment = new EducationFragment();
-                        fragmentTransaction.add(R.id.frag1,fragment);
+                        fragmentTransaction.replace(R.id.frag1,fragment);
+                        fragmentTransaction.addToBackStack(null);
+                        fragmentTransaction.commit();
+                        return true;
+                    case R.id.project :
+                        mDrawerLayout.closeDrawer(GravityCompat.START);
+                        ProjectFragment frag2 = new ProjectFragment();
+                        fragmentTransaction.replace(R.id.frag1,frag2);
+                        fragmentTransaction.addToBackStack(null);
+                        fragmentTransaction.commit();
+                        return true;
+                    case R.id.skills:
+                        mDrawerLayout.closeDrawer(GravityCompat.START);
+                        SkillFragment frag3 = new SkillFragment();
+                        fragmentTransaction.replace(R.id.frag1,frag3);
+                        fragmentTransaction.addToBackStack(null);
+                        fragmentTransaction.commit();
+                        return true;
+                    case R.id.roles :
+                        mDrawerLayout.closeDrawer(GravityCompat.START);
+                        RoleFragment frag4 = new RoleFragment();
+                        fragmentTransaction.replace(R.id.frag1,frag4);
+                        fragmentTransaction.addToBackStack(null);
+                        fragmentTransaction.commit();
+                        return true;
+                    case R.id.work :
+                        mDrawerLayout.closeDrawer(GravityCompat.START);
+                        WorkFragment frag5 = new WorkFragment();
+                        fragmentTransaction.replace(R.id.frag1,frag5);
+                        fragmentTransaction.addToBackStack(null);
+                        fragmentTransaction.commit();
+                        return true;
+                    case R.id.achievements :
+                        mDrawerLayout.closeDrawer(GravityCompat.START);
+                        AchievementFragment frag6 = new AchievementFragment();
+                        fragmentTransaction.replace(R.id.frag1,frag6);
+                        fragmentTransaction.addToBackStack(null);
+                        fragmentTransaction.commit();
+                        return true;
+                    case R.id.contact :
+                        mDrawerLayout.closeDrawer(GravityCompat.START);
+                        ContactFragment frag7 = new ContactFragment();
+                        fragmentTransaction.replace(R.id.frag1,frag7);
+                        fragmentTransaction.addToBackStack(null);
                         fragmentTransaction.commit();
                         return true;
                 }
@@ -67,6 +119,28 @@ public class MainActivity extends AppCompatActivity {
                 return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    public void project_open(View v){
+        Intent intent = new Intent(Intent.ACTION_VIEW,Uri.parse("http://www.github.com/Gayathri23599"));
+        startActivity(Intent.createChooser(intent,"Complete action using"));
+    }
+    public void ph_open(View v){
+        Intent intent = new Intent(Intent.ACTION_DIAL,Uri.parse("tel:+919445795612"));
+        startActivity(intent);
+    }
+    public void fb_open(View v){
+        Intent intent = new Intent(Intent.ACTION_VIEW,Uri.parse("http://www.facebook.com/gayathri.23599"));
+        startActivity(Intent.createChooser(intent,"Complete action using"));
+    }
+    public void mail_open(View v){
+        Intent intent = new Intent(Intent.ACTION_SEND);
+        intent.setType("message/rfc822");
+        intent.setData(Uri.parse("iamanifs@gmail.com"));
+        //intent.setClassName("com.google.android.gm","com.google.android.gm.ComposeActivityGmail");
+        intent.setPackage("com.google.android.gm");
+        intent.putExtra(Intent.EXTRA_EMAIL,new String[] { "iamanifs@gmail.com" });
+        startActivity(Intent.createChooser(intent,"Complete action using"));
     }
 
 }
---
+++ app/src/main/java/com/example/gayathri/resume/ProjectFragment.java
@@ -0,0 +1,20 @@
+package com.example.gayathri.resume;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by gayathri on 4/3/18.
+ */
+
+public class ProjectFragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.project_layout, container, false);
+    }
+}
